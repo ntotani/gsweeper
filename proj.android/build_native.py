5e2d2534710b114b843bb0821271344a8e600d56
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-03-14 13:31:11 +0900

    trim tab to space

--- proj.android/build_native.py
+++ proj.android/build_native.py
@@ -9,21 +9,21 @@ import shutil
 from optparse import OptionParser
 
 def get_num_of_cpu():
-	''' The build process can be accelerated by running multiple concurrent job processes using the -j-option.
-	'''
-	try:
-		platform = sys.platform
-		if platform == 'win32':
-			if 'NUMBER_OF_PROCESSORS' in os.environ:
-				return int(os.environ['NUMBER_OF_PROCESSORS'])
-			else:
-				return 1
-		else:
-			from numpy.distutils import cpuinfo
-			return cpuinfo.cpu._getNCPUs()
-	except Exception:
-		print "Can't know cpuinfo, use default 1 cpu"
-		return 1
+    ''' The build process can be accelerated by running multiple concurrent job processes using the -j-option.
+    '''
+    try:
+        platform = sys.platform
+        if platform == 'win32':
+            if 'NUMBER_OF_PROCESSORS' in os.environ:
+                return int(os.environ['NUMBER_OF_PROCESSORS'])
+            else:
+                return 1
+        else:
+            from numpy.distutils import cpuinfo
+            return cpuinfo.cpu._getNCPUs()
+    except Exception:
+        print "Can't know cpuinfo, use default 1 cpu"
+        return 1
 
 def check_environment_variables_sdk():
     ''' Checking the environment ANDROID_SDK_ROOT, which will be used for building
@@ -78,12 +78,10 @@ def do_build(cocos_root, ndk_root, app_android_root,ndk_build_param,sdk_root,and
         ndk_module_path = 'NDK_MODULE_PATH=%s;%s/external;%s/cocos' % (cocos_root, cocos_root, cocos_root)
     else:
         ndk_module_path = 'NDK_MODULE_PATH=%s:%s/external:%s/cocos' % (cocos_root, cocos_root, cocos_root)
-	
     num_of_cpu = get_num_of_cpu()
     build_mode_param = 'NDK_DEBUG=1'
     if build_mode == 'release':
         build_mode_param = 'NDK_DEBUG=0 NDK_APPLICATION_MK=jni/Application_release.mk'
-	
     if ndk_build_param == None:
         command = '%s -j%d -C %s %s %s' % (ndk_path, num_of_cpu, app_android_root, ndk_module_path, build_mode_param)
     else:
@@ -91,17 +89,17 @@ def do_build(cocos_root, ndk_root, app_android_root,ndk_build_param,sdk_root,and
     if os.system(command) != 0:
         raise Exception("Build dynamic library for project [ " + app_android_root + " ] fails!")
     elif android_platform is not None:
-    	  sdk_tool_path = os.path.join(sdk_root, "tools/android")
-    	  cocoslib_path = os.path.join(cocos_root, "cocos/2d/platform/android/java")
-    	  command = '%s update lib-project -t %s -p %s' % (sdk_tool_path,android_platform,cocoslib_path) 
-    	  if os.system(command) != 0:
-    	  	  raise Exception("update cocos lib-project [ " + cocoslib_path + " ] fails!")  	  
-    	  command = '%s update project -t %s -p %s -s' % (sdk_tool_path,android_platform,app_android_root)
-    	  if os.system(command) != 0:
-    	  	  raise Exception("update project [ " + app_android_root + " ] fails!")    	  	  
-    	  buildfile_path = os.path.join(app_android_root, "build.xml")
-    	  command = 'ant clean %s -f %s -Dsdk.dir=%s' % (build_mode,buildfile_path,sdk_root)
-    	  os.system(command)
+        sdk_tool_path = os.path.join(sdk_root, "tools/android")
+        cocoslib_path = os.path.join(cocos_root, "cocos/2d/platform/android/java")
+        command = '%s update lib-project -t %s -p %s' % (sdk_tool_path,android_platform,cocoslib_path) 
+        if os.system(command) != 0:
+            raise Exception("update cocos lib-project [ " + cocoslib_path + " ] fails!")
+        command = '%s update project -t %s -p %s -s' % (sdk_tool_path,android_platform,app_android_root)
+        if os.system(command) != 0:
+            raise Exception("update project [ " + app_android_root + " ] fails!")
+        buildfile_path = os.path.join(app_android_root, "build.xml")
+        command = 'ant clean %s -f %s -Dsdk.dir=%s' % (build_mode,buildfile_path,sdk_root)
+        os.system(command)
 
 def copy_files(src, dst):
 
@@ -141,15 +139,14 @@ def build(ndk_build_param,android_platform,build_mode):
     copy_resources(app_android_root)
     
     if android_platform is not None:
-				sdk_root = check_environment_variables_sdk()
-				if android_platform.isdigit():
-						android_platform = 'android-'+android_platform
-				else:
-						print 'please use vaild android platform'
-						exit(1)
-		
+        sdk_root = check_environment_variables_sdk()
+        if android_platform.isdigit():
+            android_platform = 'android-'+android_platform
+        else:
+            print 'please use vaild android platform'
+            exit(1)
     if build_mode is None:
-    	  build_mode = 'debug'
+        build_mode = 'debug'
     elif build_mode != 'release':
         build_mode = 'debug'
     
@@ -157,7 +154,6 @@ def build(ndk_build_param,android_platform,build_mode):
 
 # -------------- main --------------
 if __name__ == '__main__':
-
     parser = OptionParser()
     parser.add_option("-n", "--ndk", dest="ndk_build_param", help='parameter for ndk-build')
     parser.add_option("-p", "--platform", dest="android_platform", 
@@ -165,5 +161,4 @@ if __name__ == '__main__':
     parser.add_option("-b", "--build", dest="build_mode", 
     help='the build mode for java project,debug[default] or release.Get more information,please refer to http://developer.android.com/tools/building/building-cmdline.html')
     (opts, args) = parser.parse_args()
-    
     build(opts.ndk_build_param,opts.android_platform,opts.build_mode)
