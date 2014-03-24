c68fd83dffb0a948b1f0cdc7050f935f76b3524e
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-03-24 14:13:15 +0900

    android x86

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -16,6 +16,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
 
 
 include $(BUILD_SHARED_LIBRARY)
@@ -23,3 +24,4 @@ include $(BUILD_SHARED_LIBRARY)
 $(call import-module,2d)
 $(call import-module,audio/android)
 $(call import-module,Box2D)
+$(call import-module,editor-support/cocosbuilder)
