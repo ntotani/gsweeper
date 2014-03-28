7b154b88f2e23245c2d8a5f64afeea4186f38c6e
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-03-28 16:46:16 +0900

    ios gameplatform

---
+++ proj.ios_mac/ios/GamePlatform_objc.m
@@ -0,0 +1,45 @@
+#import "GamePlatform_objc.h"
+
+@implementation GamePlatform_objc
+
+-(void)show {
+    GKLocalPlayer *player = [GKLocalPlayer localPlayer];
+    player.authenticateHandler = ^(UIViewController* ui, NSError* error)
+    {
+        if( nil != ui )
+        {
+            [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:ui animated:YES completion:nil];
+        }
+        else if(player.isAuthenticated)
+        {
+            GKGameCenterViewController *gcView = [GKGameCenterViewController new];
+            if (gcView != nil)
+            {
+                gcView.gameCenterDelegate = self;
+                gcView.viewState = GKGameCenterViewControllerStateLeaderboards;
+                [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:gcView animated:YES completion:nil];
+            }
+        }
+    };
+}
+
+- (void)gameCenterViewControllerDidFinish:(GKGameCenterViewController *)gameCenterViewController
+{
+    [[UIApplication sharedApplication].keyWindow.rootViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)report:(int)score
+{
+    if ([GKLocalPlayer localPlayer].isAuthenticated) {
+        NSDictionary *ids = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"ids" ofType:@"plist"]][@"Root"];
+        GKScore* gkScore = [[GKScore alloc] initWithLeaderboardIdentifier:ids[@"leaderboard"]];
+        gkScore.value = score;
+        [GKScore reportScores:@[gkScore] withCompletionHandler:^(NSError *error) {
+            if (error) {
+                // エラーの場合
+            }
+        }];
+    }
+}
+
+@end
